f1313ea4b2cc58141a88e164d771c0fdf15ad974
Mrunal Tamhane <mrunal.tamhane7@gmail.com>
2024-03-22 18:05:00 +0530

    patient login signup done with landing main page

---
+++ app/src/main/java/com/example/edermacarelatestt/LoginPatientActivity.java
@@ -0,0 +1,116 @@
+package com.example.edermacarelatestt;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class LoginPatientActivity extends AppCompatActivity {
+
+    private static final String TAG = "PatientLoginActivity";
+    private EditText emailEditText, passwordEditText;
+    private Button loginButton;
+    private TextView signupRedirectText;
+
+    private FirebaseAuth mAuth;
+    private FirebaseFirestore mFirestore;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_patient);
+
+        emailEditText = findViewById(R.id.patient_login_email);
+        passwordEditText = findViewById(R.id.patient_login_password);
+        loginButton = findViewById(R.id.patient_login_button);
+        signupRedirectText = findViewById(R.id.signupredirect);
+
+        mAuth = FirebaseAuth.getInstance();
+        mFirestore = FirebaseFirestore.getInstance();
+
+        loginButton.setOnClickListener(v -> loginUser());
+        signupRedirectText.setOnClickListener(v -> redirectToSignup());
+    }
+
+    private void redirectToSignup() {
+        Intent intent = new Intent(LoginPatientActivity.this, SignUpPatientActivity.class);
+        startActivity(intent);
+    }
+
+    private void loginUser() {
+        String email = emailEditText.getText().toString().trim();
+        String password = passwordEditText.getText().toString().trim();
+
+        if (TextUtils.isEmpty(email)) {
+            Toast.makeText(getApplicationContext(), "Enter email address!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if (TextUtils.isEmpty(password)) {
+            Toast.makeText(getApplicationContext(), "Enter password!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        // Check if email is present in Firestore
+        mFirestore.collection("patients")
+                .whereEqualTo("Email", email)
+                .get()
+                .addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+                        for (DocumentSnapshot document : task.getResult()) {
+                            // Email is present in Firestore, now hash the password
+                            String hashedPassword = hashPassword(password);
+                            String storedPassword = document.getString("Password"); // Assuming "password" is the field storing hashed passwords
+
+                            if (hashedPassword.equals(storedPassword)) {
+                                // Password matches, login successful
+                                Toast.makeText(LoginPatientActivity.this, "Login Successful!", Toast.LENGTH_SHORT).show();
+                                // Proceed with further actions (e.g., navigating to another activity)
+                                return;
+                            }
+                        }
+                        // If loop completes without finding a matching email or password, login failed
+                        Toast.makeText(LoginPatientActivity.this, "Invalid email or password!", Toast.LENGTH_SHORT).show();
+                    } else {
+                        Log.d(TAG, "Error getting documents: ", task.getException());
+                        Toast.makeText(LoginPatientActivity.this, "An error occurred. Please try again later.", Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
+
+    public static String hashPassword(String password) {
+        try {
+            // Create MessageDigest instance for SHA-256
+            MessageDigest md = MessageDigest.getInstance("SHA-256");
+            // Add password bytes to digest
+            md.update(password.getBytes());
+            // Get the hash's bytes
+            byte[] bytes = md.digest();
+            // Convert bytes to hexadecimal format
+            StringBuilder sb = new StringBuilder();
+            for (byte aByte : bytes) {
+                sb.append(Integer.toString((aByte & 0xff) + 0x100, 16).substring(1));
+            }
+            // Get complete hashed password in hex format
+            return sb.toString();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
---
+++ app/src/main/java/com/example/edermacarelatestt/PatientSignUpManager.java
@@ -0,0 +1,76 @@
+package com.example.edermacarelatestt;
+import java.util.*;
+import android.app.DatePickerDialog;
+import android.content.Context;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.Toast;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import java.util.Calendar;
+
+
+
+public class PatientSignUpManager {
+
+    private DatabaseReference databaseReference;
+
+    public PatientSignUpManager() {
+        // Initialize Firebase
+
+    }
+
+    public  void showDatePickerDialog(final Context context, final EditText editTextDOB) {
+        final Calendar calendar = Calendar.getInstance();
+        int year = calendar.get(Calendar.YEAR);
+        int month = calendar.get(Calendar.MONTH);
+        int dayOfMonth = calendar.get(Calendar.DAY_OF_MONTH);
+
+        DatePickerDialog datePickerDialog = new DatePickerDialog(context,
+                new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                        String selectedDate = dayOfMonth + "/" + (month + 1) + "/" + year;
+                        editTextDOB.setText(selectedDate);
+                    }
+                }, year, month, dayOfMonth);
+
+        datePickerDialog.show();
+    }
+
+    public void signUp(Context context, String name, String email, String dob, String mobile, String hashedpassword) {
+        if (!name.isEmpty() && !email.isEmpty() && !hashedpassword.isEmpty() && !dob.isEmpty() && !mobile.isEmpty()) {
+            // Create a Firestore instance
+            FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+            // Create a Map to store patient data
+            Map<String, Object> patientData = new HashMap<>();
+            patientData.put("Name", name);
+            patientData.put("Email", email);
+            patientData.put("DOB", dob);
+            patientData.put("Mobile", mobile);
+            patientData.put("Password", hashedpassword);
+
+            // Add patient data to Firestore
+            db.collection("patients")
+                    .add(patientData)
+                    .addOnSuccessListener(documentReference -> {
+                        // Display success message
+                        Toast.makeText(context, "Sign up successful!", Toast.LENGTH_SHORT).show();
+                    })
+                    .addOnFailureListener(e -> {
+                        // Display error message
+                        Toast.makeText(context, "Failed to sign up: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+                    });
+        } else {
+            // Handle case when any field is empty
+            // Display an error message
+            Toast.makeText(context, "All fields are required!", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
+    private void displayField(Context context, String message) {
+        Toast.makeText(context, message, Toast.LENGTH_SHORT).show();
+    }
+}
