06f6f6aba78ba0f5c2b7f49a677edca6f488b642
Mrunal Tamhane <mrunal.tamhane7@gmail.com>
2024-05-03 12:50:04 +0530

    populated dermatologist fragment

--- app/src/main/java/com/example/edermacarelatestt/Activitydash2.java
+++ app/src/main/java/com/example/edermacarelatestt/Activitydash2.java
@@ -53,24 +53,23 @@ public class Activitydash2 extends AppCompatActivity implements NavigationView.O
 
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
         int itemId = item.getItemId();
-        if (itemId == R.id.navhs) {
+        if (itemId == R.id.navhs2) {
             String userEmail = getIntent().getStringExtra("user_email");
-
+            System.out.println(userEmail);
             if (userEmail != null) {
                 // Pass user email to profileFragment
                 Bundle bundle = new Bundle();
                 bundle.putString("user_email", userEmail);
                 profileFragment2 fragment = new profileFragment2();
                 fragment.setArguments(bundle);
+                System.out.println(userEmail);
+                System.out.println(bundle);
                 getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, fragment).commit();
             } else {
                 // Handle the case where user email is not found
                 Toast.makeText(this, "User email not found", Toast.LENGTH_SHORT).show();
             }
 
-        }else if (itemId == R.id.navhs2) {
-            // Redirect to Dermatologist Profile Activity
-            startActivity(new Intent(Activitydash2.this,DermatologistProfile.class));
         }else  if (item.getItemId() == R.id.logout) {
             logout();
             return true;
